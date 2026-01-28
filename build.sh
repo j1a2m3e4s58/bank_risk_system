2e8ec3cf6241efd4fa258b209cfd9f1e3fa997cc
James Lincoln Awuah <lincolnawuahjames@gmail.com>
2026-01-28 16:33:42 +0000

    Add Render build script

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 #!/usr/bin/env bash
-# Exit on error
 set -o errexit
 
 pip install -r requirements.txt
-
-# Convert static files
-python manage.py collectstatic --no-input
-
-# Migrate database
+python manage.py collectstatic --noinput
 python manage.py migrate
